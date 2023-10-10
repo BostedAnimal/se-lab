2520d176b90bed256faa0d6c7d59b6ddcc98d71f
Németh Márton <nemeth.marci07@gmail.com>
2023-10-10 17:48:44 +0200

    lab

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,6 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         firingSuccess=primaryTorpedoStore.fire(1)&&secondaryTorpedoStore.fire(1);
+        int i=1;
         break;
     }
 
